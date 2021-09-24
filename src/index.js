4ff0fab8db17508d7a8516ff2191f35ccfe41e0a
FoloraD <fduang@gmail.com>
2021-09-24 17:20:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 export const projectName = () => {
   return 'Kata starter project';
 }
+
+/* Write a function that takes numbers from 1 to 15 and returns them, such that
+for multiples of 3 returns 'Fizz' instead of number
+for multiples  of 5 returns 'Buzz'
+for multiples of 15 returns 'FizzBuzz'
+*/
